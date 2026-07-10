eb78061675fd9eb68a7812cf1e9756193b5f6bda
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 22:29:16 +0530

    Create members.ts

---
+++ src/data/members.ts
@@ -0,0 +1,36 @@
+import { Member } from "@/types/member";
+
+export const members: Member[] = [
+  {
+    id: 1,
+    memberId: "DPA001",
+
+    fullName: "Admin User",
+
+    fatherName: "Ramesh",
+
+    mobile: "9876543210",
+
+    aadhaar: "123412341234",
+
+    email: "admin@gmail.com",
+
+    gender: "Male",
+
+    dob: "1990-01-01",
+
+    bloodGroup: "O+",
+
+    studioName: "Dammapeta Studio",
+
+    address: "Dammapeta",
+
+    joinDate: "2025-01-01",
+
+    photo: "",
+
+    role: "Admin",
+
+    status: "Active",
+  },
+];
