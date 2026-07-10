518c13101f870fdedf202fda004929f209b2580a
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 20:45:04 +0530

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,39 +1,21 @@
 import React from "react";
 
-import {
-useAuth
-}
-from "@/hooks/useAuth";
-
 
 export default function DashboardPage(){
 
 
-const {
-profile
-}=useAuth();
-
-
-
 return(
 
-<div>
-
+<div style={{padding:"40px"}}>
 
 <h1>
-Dashboard
+Dammapeta Photographers Dashboard
 </h1>
 
 
-<h2>
-Welcome {profile?.name}
-</h2>
-
-
 <p>
-Role : {profile?.role}
+Login Successful
 </p>
-
 
 
 </div>
