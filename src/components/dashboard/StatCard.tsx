fa7790847e0aa1952126b4e0fe235bb8641e270c
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-11 02:12:28 +0530

    Update StatCard.tsx

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -1,37 +1,21 @@
+interface Props {
+  title: string;
+  value: string | number;
+}
+
 export default function StatCard({
-title,
-value
-}:Props){
+  title,
+  value,
+}: Props) {
+  return (
+    <div className="bg-white rounded-xl shadow p-5">
+      <h3 className="text-gray-500 text-sm">
+        {title}
+      </h3>
 
-return(
-
-<div
-className="
-bg-white
-rounded-xl
-shadow
-p-5
-"
->
-
-<h3 className="text-gray-600">
-{title}
-</h3>
-
-
-<h1
-className="
-text-3xl
-font-bold
-mt-2
-"
->
-{value}
-</h1>
-
-
-</div>
-
-);
-
+      <h2 className="text-3xl font-bold mt-2">
+        {value}
+      </h2>
+    </div>
+  );
 }
