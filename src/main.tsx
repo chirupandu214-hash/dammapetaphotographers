1b29b8757002db2cb6d1c6bacc3a14bc662eed56
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 20:30:50 +0530

    Refactor AuthProvider import and JSX structure

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,26 @@ import App from "./app/App";
 
 import "./index.css";
 
-import { AuthProvider } from "./context/AuthContext";
+import {
+AuthProvider
+}
+from "@/hooks/useAuth";
+
 
 
 ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
+document.getElementById("root")!
+)
+.render(
 
-  <React.StrictMode>
+<React.StrictMode>
 
-    <AuthProvider>
+<AuthProvider>
 
-      <App />
+<App/>
 
-    </AuthProvider>
+</AuthProvider>
 
-  </React.StrictMode>
+</React.StrictMode>
 
 );
