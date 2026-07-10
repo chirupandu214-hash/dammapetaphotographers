89ceafeccab9417a22db6329b4f8bb1abbd686a6
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 20:26:41 +0530

    Create BankTransactionsPage.tsx

---
+++ src/pages/bank/BankTransactionsPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function BankTransactionsPage(){
+
+return(
+ <div style={{padding:40}}>
+
+  <h1>
+   Bank Transactions
+  </h1>
+
+  <p>
+   Transactions management
+  </p>
+
+ </div>
+);
+
+}
