21a60560994d6489204026a5238e88b89c9eefde
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 14:59:01 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "@/app/App";
-import { Providers } from "@/app/providers";
-import "@/styles/index.css";
+import App from "@/app/App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
