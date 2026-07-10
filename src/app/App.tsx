a18abb968a1c5887ec4bb76afa790b07881e98e8
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 14:58:25 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "@/app/routes";
-
-export function App() {
-  return <RouterProvider router={router} />;
+export default function App() {
+  return (
+    <div>
+      Dhammapeta Photographers Association Portal
+    </div>
+  );
 }
