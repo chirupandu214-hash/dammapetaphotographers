7e7e54525b88422229920c88b75dbbbda7403e8e
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 22:04:21 +0530

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <header
+      style={{
+        height: "60px",
+        background: "#1976d2",
+        color: "#fff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 20px",
+      }}
+    >
+      <h2>Dammapeta Photographers</h2>
+
+      <span>Admin</span>
+    </header>
+  );
+}
