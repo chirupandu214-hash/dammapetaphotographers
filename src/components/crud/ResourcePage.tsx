7880abb93f07a3ac9160fa0875af4657e7058568
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 21:21:10 +0530

    Update ResourcePage.tsx

--- src/components/crud/ResourcePage.tsx
+++ src/components/crud/ResourcePage.tsx
@@ -1,26 +1,12 @@
-import type { ResourceConfig } from "@/config/resources";
+import { useParams } from "react-router-dom";
 
-
-interface ResourcePageProps {
-  resource: ResourceConfig;
-}
-
-
-export function ResourcePage({
-  resource,
-}: ResourcePageProps) {
+export default function ResourcePage() {
+  const { resource } = useParams();
 
   return (
-    <div className="p-6">
-
-      <h1 className="text-2xl font-bold">
-        {resource.title}
-      </h1>
-
-      <p className="mt-2">
-        Manage {resource.title}
-      </p>
-
+    <div style={{ padding: 20 }}>
+      <h2>{resource}</h2>
+      <p>CRUD Page</p>
     </div>
   );
 }
