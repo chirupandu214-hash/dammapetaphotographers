b464adbc02386903c4a0c839baf9cdb89ccd4b16
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 13:52:56 +0530

    Create Input.tsx

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Input(
+  props: React.InputHTMLAttributes<HTMLInputElement>
+) {
+  return (
+    <input
+      {...props}
+      className={`border rounded px-3 py-2 w-full ${props.className || ""}`}
+    />
+  );
+}
