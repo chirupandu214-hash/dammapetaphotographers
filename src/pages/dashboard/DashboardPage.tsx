3bff0b980cd17cfb3eb880362793c6a2eabdde47
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 21:56:23 +0530

    Create DashboardPage.tsx

---
+++ src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Dashboard</h1>
+
+      <p>Welcome to Dammapeta Photographers Management Portal.</p>
+    </div>
+  );
+}
