56f2c6a301d693e6e0a73c379049bacce9e8a651
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-11 00:34:40 +0530

    Create FundPage.tsx

---
+++ src/pages/fund/FundPage.tsx
@@ -0,0 +1,28 @@
+import AppLayout
+from "@/layouts/AppLayout";
+
+
+import FundForm
+from "@/components/fund/FundForm";
+
+
+
+export default function FundPage(){
+
+return(
+
+<AppLayout>
+
+<h1>
+Yearly Fund
+</h1>
+
+
+<FundForm/>
+
+
+</AppLayout>
+
+);
+
+}
