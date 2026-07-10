b5e3144ddfa7f2ab6bed862910f5d9e795dc7d6a
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 20:25:43 +0530

    Create MemberProfilePage.tsx

---
+++ src/pages/members/MemberProfilePage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function MemberProfilePage(){
+
+return(
+ <div style={{padding:40}}>
+  <h1>
+   Member Profile
+  </h1>
+
+  <p>
+   Member details page
+  </p>
+
+ </div>
+);
+
+}
