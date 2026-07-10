0229b4c2ac60d3e1cf05840f44047c7cfd80d9b3
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 22:30:46 +0530

    Update MembersPage.tsx

--- src/pages/members/MembersPage.tsx
+++ src/pages/members/MembersPage.tsx
@@ -1,9 +1,17 @@
 import AppLayout from "@/layouts/AppLayout";
 
+import MemberTable from "@/components/members/MemberTable";
+
+import { members } from "@/data/members";
+
 export default function MembersPage() {
   return (
     <AppLayout>
       <h1>Members</h1>
+
+      <br />
+
+      <MemberTable members={members} />
     </AppLayout>
   );
 }
