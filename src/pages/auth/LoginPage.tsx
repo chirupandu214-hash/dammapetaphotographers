4dde4b7cc48ac941a13be646a8fa1010e0d51f8d
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 20:31:39 +0530

    Update LoginPage.tsx

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,26 +1,105 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React,
+{
+useState
+}
+from "react";
+
+
+import {
+supabase
+}
+from "@/lib/supabase";
+
+
+import {
+useNavigate
+}
+from "react-router-dom";
+
 
 
 export default function LoginPage(){
 
- const navigate = useNavigate();
+
+const [email,setEmail]=useState("");
+
+const [password,setPassword]=useState("");
+
+const [error,setError]=useState("");
+
+const navigate=useNavigate();
 
 
- return (
-  <div style={{padding:40}}>
 
-   <h1>
-    Dammapeta Photographers Portal
-   </h1>
+async function login(){
 
-   <button 
-    onClick={()=>navigate("/dashboard")}
-   >
-    Login
-   </button>
 
-  </div>
- );
+const {error}=await supabase.auth.signInWithPassword({
+
+email,
+password
+
+});
+
+
+if(error){
+
+setError(error.message);
+
+}
+else{
+
+navigate("/dashboard");
+
+}
+
+
+}
+
+
+
+return(
+
+<div className="login">
+
+
+<h1>
+Dammapeta Photographers Portal
+</h1>
+
+
+<input
+placeholder="Email"
+value={email}
+onChange={
+e=>setEmail(e.target.value)
+}
+/>
+
+
+<input
+type="password"
+placeholder="Password"
+value={password}
+onChange={
+e=>setPassword(e.target.value)
+}
+/>
+
+
+<button onClick={login}>
+Login
+</button>
+
+
+<p>
+{error}
+</p>
+
+
+</div>
+
+);
+
 
 }
