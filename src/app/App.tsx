3913b0415dccdde9a029cb4f000e38b67178169c
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 21:25:30 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,47 +1,6 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
-import LoginPage from "@/pages/LoginPage";
-import DashboardPage from "@/pages/DashboardPage";
-
-import MemberProfilePage from "@/pages/members/MemberProfilePage";
-import BankTransactionsPage from "@/pages/bank/BankTransactionsPage";
-import QRVerificationPage from "@/pages/qr/QRVerificationPage";
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route 
-          path="/" 
-          element={<LoginPage />} 
-        />
-
-        <Route 
-          path="/dashboard" 
-          element={<DashboardPage />} 
-        />
-
-        <Route 
-          path="/members/profile" 
-          element={<MemberProfilePage />} 
-        />
-
-        <Route 
-          path="/bank/transactions" 
-          element={<BankTransactionsPage />} 
-        />
-
-        <Route 
-          path="/qr-verification" 
-          element={<QRVerificationPage />} 
-        />
-
-      </Routes>
-    </BrowserRouter>
-  );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
