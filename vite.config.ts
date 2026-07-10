8e9fbc6677ab6de37bdf8b681d30f2c568a6528d
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 19:57:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [
+    react()
+  ],
+
   resolve: {
+
     alias: {
-      "@": path.resolve(__dirname, "./src")
+
+      "@": path.resolve(
+        "./src"
+      )
+
     }
+
   }
+
 });
