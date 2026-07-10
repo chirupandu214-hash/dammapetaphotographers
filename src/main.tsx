433dc335392c229f13e4b51f998cab6936be8786
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 19:42:13 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import App from "./app/App";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import { AuthProvider } from "./context/AuthContext";
+
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+
   <React.StrictMode>
-    <App />
+
+    <AuthProvider>
+
+      <App />
+
+    </AuthProvider>
+
   </React.StrictMode>
+
 );
