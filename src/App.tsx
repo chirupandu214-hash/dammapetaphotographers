82906e78fdcbc93fc26d4f63756bdf545ef7bf58
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-10 17:53:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ padding: "20px", fontSize: "30px" }}>
-      ✅ Dammapeta Portal Working
+      Dammapeta Photographers Manage Portal
     </div>
   );
 }
