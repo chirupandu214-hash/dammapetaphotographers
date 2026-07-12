d517c150717c09c895161c229050361c7ab34390
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-12 09:28:27 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
-<Route path="/members" element={
-  <ProtectedRoute allowedRole="super_admin">
-    <MembersPage />
-  </ProtectedRoute>
-} />
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <HashRouter>
+        <Routes>
+          {/* మీ రూట్స్ */}
+        </Routes>
+      </HashRouter>
+    </AuthProvider>
+  );
+}
