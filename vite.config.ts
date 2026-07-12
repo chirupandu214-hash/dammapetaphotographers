a636a213e2c869c4712d2db44a4271313a263504
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-12 09:23:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ఒకవేళ ఈ కింద ఉన్న లైన్ లేకపోతే, దీన్ని యాడ్ చేసి గిట్‌హబ్‌కు పుష్ చేయండి
-  base: '/', 
-})
+  base: './', // ఇది లేకపోతే సర్వర్ లో CSS ఫైల్స్ లోడ్ అవ్వవు
+});
