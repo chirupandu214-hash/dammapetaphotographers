81f486a5259fed3254c9c83e6c317b58a7627320
chirupandu214-hash <chirupandu214@gmail.com>
2026-07-12 10:36:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ఇది లేకపోతే సర్వర్ లో CSS ఫైల్స్ లోడ్ అవ్వవు
+  server: {
+    port: 5173,
+    open: true
+  }
 });
